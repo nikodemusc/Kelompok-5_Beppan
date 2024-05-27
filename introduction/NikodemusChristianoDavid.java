2fc5464fd3fbacadfb72e1eed32c2cb9ff394c05
nikodemusc <169017601+nikodemusc@users.noreply.github.com>
2024-05-27 09:09:58 +0700

    Add files via upload

---
+++ introduction/NikodemusChristianoDavid.java
@@ -0,0 +1,16 @@
+
+public class NikodemusChristianoDavid {
+
+    public static void main(String[] args) {
+        String fullname = "Nikodemus Christiano David";
+        String nim = "2213010361";
+        String team = "Beppan";
+        String role = "Designer";
+
+        System.out.println("Halo, perkenalkan nama saya " + fullname + ".");
+        System.out.println("Nim saya = " + nim + ".");
+        System.out.println("Saya tergabung dalam kelompok " + team + ".");
+        System.out.println("Peran saya disini sebagai " + role + ".");
+    }
+
+}
