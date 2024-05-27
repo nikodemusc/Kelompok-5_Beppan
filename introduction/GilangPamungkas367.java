f3cd6bccba5fd22b7e5b9d5b959af058bf7ae06c
SiwoMikza <169017319+SiwoMikza@users.noreply.github.com>
2024-05-27 09:49:08 +0700

    Create GilangPamungkas367.java

---
+++ introduction/GilangPamungkas367.java
@@ -0,0 +1,16 @@
+
+public class GilangPamungkas {
+
+    public static void main(String[] args) {
+        String fullname = "Gilang Pamungkas";
+        String nim = "2213010367";
+        String team = "Beppan";
+        String role = "Leader";
+
+        System.out.println("Halo, perkenalkan nama saya " + fullname + ".");
+        System.out.println("Nim saya = " + nim + ".");
+        System.out.println("Saya tergabung dalam kelompok " + team + ".");
+        System.out.println("Peran saya disini sebagai " + role + ".");
+    }
+
+}
