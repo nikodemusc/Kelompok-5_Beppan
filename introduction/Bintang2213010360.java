853561cbae5dcebd00cc5f69ede204683b22bf37
bintangptrarya <169017346+bintangptrarya@users.noreply.github.com>
2024-05-26 21:19:45 -0700

    Create Bintang2213010360.java

---
+++ introduction/Bintang2213010360.java
@@ -0,0 +1,17 @@
+
+
+public class Bintangputraaryatama{
+
+    public static void main(String[] args) {
+        String fullname = "Bintang Putra Aryatama";
+        String nim = "2213010360";
+        String team = "Beppan";
+        String role = "Markerter";
+
+        System.out.println("Halo, perkenalkan nama saya " + fullname + ".");
+        System.out.println("Nim saya = " + nim + ".");
+        System.out.println("Saya tergabung dalam kelompok " + team + ".");
+        System.out.println("Peran saya disini sebagai " + role + ".");
+    }
+
+}
