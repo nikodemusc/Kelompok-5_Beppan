180af74105b86a3a576cce3acc0dc1d27c995258
rizky-anggara <162684816+rizky-anggara@users.noreply.github.com>
2024-05-27 10:04:06 +0700

    2213010372 (Muh Rizky Anggara Yunan Putra)
    2213010372 (Muh Rizky Anggara Yunan Putra)

---
+++ introduction/MuhRizkyAnggaraYunanPutra.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package utspenilaianpribadi;
+
+/**
+ *
+ * @author user
+ */
+public class MuhRizkyAnggaraYunanPutra {
+    public static void main(String[] args) {
+        String fullname = "Muh Rizky Anggara Yunan Putra";
+        String nim = "2213010372";
+        String team = "Beppan";
+        String role = "Programmer";
+
+        System.out.println("Halo, perkenalkan nama saya " + fullname + ".");
+        System.out.println("Nim saya = " + nim + ".");
+        System.out.println("Saya tergabung dalam kelompok " + team + ".");
+        System.out.println("Peran saya disini sebagai " + role + ".");
+    }
+}
